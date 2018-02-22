14de65fce1a889352d9bcf73ea06724e68de4090
Santosh Botre <>
2018-02-22 10:10:48 +0530

    Navigator project has been created to showcase the design approach for the screen navigation

---
+++ Navigator/Navigator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigator
+//
+//  Created by Santosh Botre on 22/02/18.
+//  Copyright © 2018 Santosh Botre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
